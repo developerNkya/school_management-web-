cee2d594c4848705281578d57bfb24c28ec504af
developernkya <developernkya@gmail.com>
2024-08-07 21:15:08 +0300

    20240807 | INITIAL COMMIT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function userLogin(Request $request)
+    {
+        $rules = [
+            'email' => 'required',
+            'password' => 'required',
+        ];
+
+        $validator = validator::make($request->all(), $rules);
+        $error = $validator->errors()->first();
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $error
+            ]);
+        }
+
+        $verifier = User::where('email', $request->email)
+            ->where('password', $request->password)
+            ->first();
+
+
+        if (!$verifier) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry...Incorrect Email or Password!',
+            ]);
+        }
+
+        //check role:
+        switch ($verifier->role_id) {
+            case 1:
+                return redirect('super_admin/');
+                // return view('super_admin.index');
+                break;
+
+            default:
+                # code...
+                break;
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SuperAdminController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::post('/user-login',  [AuthController::class, 'userLogin']);
+
+
+//for super admins
+Route::name('super_admin.')->prefix('super_admin')->group(function() {
+    Route::get('/',  [SuperAdminController::class, 'index']);
+
+});
