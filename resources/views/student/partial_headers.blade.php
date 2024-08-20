c3394d2f59bd053accc9b0c8f12b95a2884680f2
developernkya <developernkya@gmail.com>
2024-08-20 19:16:04 +0300

    20240820 | MODIFIED THE FOOTER SECTIONS

--- resources/views/student/partial_headers.blade.php
+++ resources/views/student/partial_headers.blade.php
@@ -79,53 +79,8 @@
       <!-- partial -->
       <div class="container-fluid page-body-wrapper">
         <!-- partial:partials/_sidebar.html -->
-        {{-- <nav class="sidebar sidebar-offcanvas" id="sidebar">
-          <ul class="nav">
-            <li class="nav-item">
-              <a class="nav-link" href="{{ url('school_admin/') }}">
-                <i class="mdi mdi-grid-large menu-icon"></i>
-                <span class="menu-title">Dashboard</span>
-              </a>
-            </li>
-            <li class="nav-item nav-category">CONTENTS</li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{ url('school_admin/classes') }}">
-                <i class="mdi mdi-grid-large menu-icon"></i>
-                <span class="menu-title">Classes</span>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
-                <i class="menu-icon mdi mdi-card-text-outline"></i>
-                <span class="menu-title">Form elements</span>
-                <i class="menu-arrow"></i>
-              </a>
-              <div class="collapse" id="form-elements">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
-                </ul>
-              </div>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="{{'/school_admin/students'}}">
-                <i class="mdi mdi-grid-large menu-icon"></i>
-                <span class="menu-title">Students</span>
-              </a>
-            </li>
-
-          </ul>
-        </nav> --}}
-
         <nav class="sidebar sidebar-offcanvas" id="sidebar">
           <ul class="nav">
-            <li class="nav-item">
-              <a class="nav-link" href="{{ url('school_admin/') }}">
-                <i class="mdi mdi-grid-large menu-icon"></i>
-                <span class="menu-title">Dashboard</span>
-              </a>
-            </li>
             <li class="nav-item nav-category">Contents</li>
             <li class="nav-item">
               <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
