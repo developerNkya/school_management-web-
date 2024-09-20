f0e9862e5a41123c01002d7000297b7bd2fe9edf
developernkya <developernkya@gmail.com>
2024-09-20 11:30:01 +0300

    20240920 | ADDED ICON IN SIGN PAGE

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,9 +29,9 @@
           <div class="row w-100 mx-0">
             <div class="col-lg-4 mx-auto">
               <div class="auth-form-light text-left py-5 px-4 px-sm-5">
-                {{-- <div class="brand-logo">
-                  <img src="../../assets/images/logo.svg" alt="logo">
-                </div> --}}
+                <div class="brand-logo" style="margin-left: 30%">
+                  <img src="../../assets/images/logo-icon.png" alt="logo">
+                </div>
                 <h6 class="fw-light"><b>Sign in to continue.</b></h6>
                 @include('helpers.message_handler')
 
